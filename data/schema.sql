b2530d3b4ba9aae00b75042eac6967e067a9f44b
Jonathan Meyer <jonathan.meyer@libertymutual.com>
2019-06-16 15:47:59 -0400

    database

--- data/schema.sql
+++ data/schema.sql
@@ -3,11 +3,12 @@ create database if not exists `bamazon`;
 use `bamazon`;
 
 CREATE TABLE `products` (
-    `item_id` INT NOT NULL AUTO_INCREMENT,
-    `product_name` VARCHAR(45) NULL,
-    `department_name` VARCHAR(45) NULL,
-    `price` DECIMAL(10,2) NULL,
-    `stock_quantity` VARCHAR(45) NULL,
     
+    `item_id` INT NOT NULL AUTO_INCREMENT,
+    `product_name` VARCHAR(255) NULL,
+    `department_name` VARCHAR(255) NULL,
+    `price` DECIMAL(10,2) NULL,
+    `stock_quantity` INT NOT NULL DEFAULT 0,    
+
     PRIMARY KEY (`item_id`)
 );
---
+++ data/seed.sql
@@ -0,0 +1,16 @@
+use `bamazon`;
+
+INSERT INTO `bamazon`.`products` 
+    (`product_name`, `department_name`, `price`, `stock_quantity`) 
+VALUES 
+    ('Moxie', 'Food and Drink', '2.25', '10'),
+    ('Coke', 'Food and Drink', '1.25', '10'),
+    ('t-shirt', 'Clothing', '8.95', '50'),
+    ('pants', 'Clothing', '13.95', '50'),
+    ('pots', 'Cookware', '9.95', '20'),
+    ('pans', 'Cookware', '19.95', '20'),
+    ('pens', 'Stationery', '1.95', '200'),
+    ('paper', 'Stationery', '5.95', '100'),
+    ('xbox360', 'Electronics', '455.95', '10'),
+    ('Wii-U', 'Electronics', '255.95', '10')
+;
