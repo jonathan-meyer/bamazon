b87c1d92f03e1cf892b02cf3e4658ecfbe0f02fb
Jonathan Meyer <jonathan.meyer@libertymutual.com>
2019-06-17 16:30:32 -0400

    query for input form customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,13 @@
 const Table = require("cli-table");
+const inquirer = require("inquirer");
+
 const Data = require("./lib/Data");
+const Product = require("./lib/Product");
 
 (async () => {
   const data = new Data();
   const products = await data.products();
+
   const table = new Table({
     head: ["Id", "Name", "Dept", "Price", "Qty"],
     colAligns: [, , , "right", "right"]
@@ -19,5 +23,31 @@ const Data = require("./lib/Data");
     ]);
   });
 
+  console.log(Number("1"), 1, Number("1") === 1);
+
+  // display the table
   console.log(table.toString());
+
+  // prompt for product to buy
+  const { product, units } = await inquirer.prompt([
+    {
+      name: "product",
+      message: "Which product do you want to buy:",
+      filter: input =>
+        products.filter(product => product.id === Number(input)).pop(),
+      validate: input =>
+        input instanceof Product ? true : "Cannot find that product.",
+      transformer: input => input.name || input
+    },
+    {
+      name: "units",
+      message: "How many do you want to buy:",
+      validate: (input, answers) =>
+        answers.product.quantity >= Number(input)
+          ? true
+          : "We don't have that many in stock."
+    }
+  ]);
+
+  console.log({ product, units });
 })();
